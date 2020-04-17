4779c8da706475a1181ef69a2f3e5142fe000563
Joey Reyes <joeyr1984@gmail.com>
2020-04-17 11:00:17 -0700

    adding detail to read me and adding prompts to .js file

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # password-generator
+
+
+The intended functionallity of this web app is to promt the user with a series of questions to create a password 
+
+There inputs should be saved and leveraged to create a password comprised of :
+
+ - Random letters
+ - Upper Case
+ - Lower case 
+ - Special characters  
--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var passwordLength = prompt("Pick a number between 8-128");
+
+var passwordUpper = prompt("Do you want upper case...y or n?")
+if (passwordUpper =="yes")
+
+var passwordLower = prompt("Do you want lower case...y or n?")
+if (passwordUpper =="yes")
+
+var specialChar = prompt("Do you want special characters?")
+if (specialChar =="yes")
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+
 
   passwordText.value = password;
 
@@ -12,3 +24,11 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+
+
+console.log(passwordLength)
+console.log(passwordUpper)
+console.log(passwordLower)
+console.log(specialChar)
