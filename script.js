a46229e2677ad998c7b64c0b079767db035d4ed5
Joey Reyes <joeyr1984@gmail.com>
2020-04-17 19:16:56 -0700

    fixing prompts with do while loops

--- script.js
+++ script.js
@@ -2,28 +2,38 @@
 var generateBtn = document.querySelector("#generate");
 
 // User Promts
-var passwordLength = 0;
-do {
-  passwordLength = prompt("Pick a number between 8-128");
-}while(passwordLength < 8 || passwordLength > 128);
+function generatePassword() {
+  var passwordLength = 0;
+  do {
+    passwordLength = prompt("Pick a number between 8-128");
+  } while (passwordLength < 8 || passwordLength > 128);
 
-var numbers = prompt("Y or N ...Do you want your password to include numbers?")
-if (numbers =="y" || numbers =="Y") {
-  
-  console.log("this is a yes")
+  var numbers = "";
+  do {
+    numbers = prompt("Y or N ...Do you want your password to include numbers?");
+  } while (numbers != "Y" && numbers != "N");
+
+
+  var passwordUpper = "";
+  do {
+    passwordUpper = prompt("Y or N ...Do you want your password to include Uppercase?");
+  } while (passwordUpper != "Y" && passwordUpper != "N");
+
+  var passwordLower = "";
+  do {
+    passwordLower = prompt("Y or N ...Do you want your password to include Lowercase?");
+  } while (passwordLower != "Y" && passwordLower != "N");
+
+  var specialChar = "";
+  do {
+    specialChar = prompt("Y or N ...Do you want your password to include specialChar?");
+  } while (specialChar != "Y" && specialChar != "N");
+  console.log(passwordLength)
+  console.log(numbers);
+  console.log(passwordUpper);
+  console.log(passwordLower);
+  console.log(specialChar);
 }
-else if (numbers =="n" || numbers =="N"){
-
-}
-var passwordUpper = prompt("Y or N ...Do you want upper case")
-//if (passwordUpper =="y")
-
-var passwordLower = prompt("Y or N ...Do you want lower case")
-//if (passwordLower =="y")
-
-var specialChar = prompt("Y or N ...Do you want special characters?")
-//if (specialChar =="y")
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -40,7 +50,3 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-console.log(passwordLength)
-console.log(passwordUpper)
-console.log(passwordLower)
-console.log(specialChar)
