31d22491b6b50ee7818ec8c874fc28c309fd699d
Joey Reyes <joeyr1984@gmail.com>
2020-04-17 21:03:37 -0700

    added alert and commented out my code

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// THESE ARE ARRAYS THAT CONTAIN ASCII CODES
 const LOWERCASE_CHAR_CODES = arrayFromLowToHigh(97, 122)
 const UPPERCASE_CHAR_CODES = arrayFromLowToHigh(65, 90)
 const NUMBER_CHAR_CODES = arrayFromLowToHigh(48, 57)
 const SYMBOL_CHAR_CODES = arrayFromLowToHigh(33, 47).concat(
-    arrayFromLowToHigh(58, 64)
-    ).concat(
-        arrayFromLowToHigh(91, 96)
-    ).concat(
-        arrayFromLowToHigh(124, 126)
-    )
- 
-// User Promts
-function userPrompts(){
+  arrayFromLowToHigh(58, 64)
+).concat(
+  arrayFromLowToHigh(91, 96)
+).concat(
+  arrayFromLowToHigh(124, 126)
+)
+
+// User Prompts retruns object with user responses. If USER says no to everything we return nothing to later send a alert message to user
+function userPrompts() {
   var passwordLength = 0;
   do {
     passwordLength = prompt("Pick a number between 8-128");
@@ -38,31 +39,40 @@ function userPrompts(){
   do {
     specialChar = prompt("Y or N ...Do you want your password to include specialChar?");
   } while (specialChar != "Y" && specialChar != "N");
-return {
-  passwordLength: passwordLength,
-  numbers: numbers,
-  passwordUpper: passwordUpper,
-  passwordLower: passwordLower,
-  specialChar: specialChar
-   }
+  if (numbers == "N" && passwordUpper == "N" && passwordLower == "N" && specialChar == "N") {
+       return null
+  } else {
+    return {
+      passwordLength: passwordLength,
+      numbers: numbers,
+      passwordUpper: passwordUpper,
+      passwordLower: passwordLower,
+      specialChar: specialChar
+    }
+  }
+
 }
+//THIS GENERATES PASSWORD BASED OFF USER RESPONSES. CREATES MASTER ARRAY OF CRITERIA TO GENERATE PASSWORD 
 function generatePassword() {
   var passwordCriteria = userPrompts();
-  console.log(passwordCriteria);
+  console.log(passwordCriteria)
+if (passwordCriteria ==null){
+ return "WARNING you must have at least one 'Y' click generate password again"; 
+}else {
   var asciiCodes = [];
-  if (passwordCriteria.numbers =="Y"){
+  if (passwordCriteria.numbers == "Y") {
     asciiCodes = asciiCodes.concat(NUMBER_CHAR_CODES);
   }
-  if (passwordCriteria.passwordUpper =="Y"){
+  if (passwordCriteria.passwordUpper == "Y") {
     asciiCodes = asciiCodes.concat(UPPERCASE_CHAR_CODES);
-  } 
-  if (passwordCriteria.passwordLower =="Y"){
+  }
+  if (passwordCriteria.passwordLower == "Y") {
     asciiCodes = asciiCodes.concat(LOWERCASE_CHAR_CODES);
-  } 
-  if (passwordCriteria.specialChar =="Y"){
+  }
+  if (passwordCriteria.specialChar == "Y") {
     asciiCodes = asciiCodes.concat(SYMBOL_CHAR_CODES);
-  } 
-  
+  }
+
   const passwordCharacters = []
   for (let i = 0; i < passwordCriteria.passwordLength; i++) {
     const characterCode = asciiCodes[Math.floor(Math.random() * asciiCodes.length)];
@@ -70,21 +80,21 @@ function generatePassword() {
   }
   return passwordCharacters.join('')
 }
+  
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
-
   passwordText.value = password;
-
 }
-function arrayFromLowToHigh(low, high){
-  const array = []
-  for (let i = low; i <= high; i++){
-  array.push(i)
-}
-return array
+//HELP FOR FUNCTION TO CREATE ARRAYS FOR ASCII CHARACTER GROUPS
+function arrayFromLowToHigh(low, high) {
+  const array = [];
+  for (let i = low; i <= high; i++) {
+    array.push(i);
+  }
+  return array;
 }
 
 // Add event listener to generate button
